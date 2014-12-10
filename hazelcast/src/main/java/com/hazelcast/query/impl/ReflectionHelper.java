079b15cf826bc65153461d59aa85f35c8d3542c2
Ryan Stephens <ryan@mach19.com>
2014-12-10 14:42:22 -0700

    First stab at Maps and dot notation

--- hazelcast/src/main/java/com/hazelcast/query/impl/ReflectionHelper.java
+++ hazelcast/src/main/java/com/hazelcast/query/impl/ReflectionHelper.java
@@ -31,61 +31,62 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 import static com.hazelcast.query.QueryConstants.THIS_ATTRIBUTE_NAME;
+import java.util.Map;
 
 /**
  * Scans your classpath, indexes the metadata, allows you to query it on runtime.
  */
 public final class ReflectionHelper {
 
-    private static final ClassLoader THIS_CL = ReflectionHelper.class.getClassLoader();
-    private static final ConcurrentMap<String, Getter> GETTER_CACHE = new ConcurrentHashMap<String, Getter>(1000);
-    private static final int INITIAL_CAPACITY = 3;
+      private static final ClassLoader THIS_CL = ReflectionHelper.class.getClassLoader();
+  private static final ConcurrentMap<String, Getter> GETTER_CACHE = new ConcurrentHashMap<String, Getter>(1000);
+  private static final int INITIAL_CAPACITY = 3;
 
-    private ReflectionHelper() {
+  private ReflectionHelper() {
+  }
+
+  public static AttributeType getAttributeType(Class klass) {
+    if (klass == String.class) {
+      return AttributeType.STRING;
+    } else if (klass == int.class || klass == Integer.class) {
+      return AttributeType.INTEGER;
+    } else if (klass == long.class || klass == Long.class) {
+      return AttributeType.LONG;
+    } else if (klass == boolean.class || klass == Boolean.class) {
+      return AttributeType.BOOLEAN;
+    } else if (klass == double.class || klass == Double.class) {
+      return AttributeType.DOUBLE;
+    } else if (klass == BigDecimal.class) {
+      return AttributeType.BIG_DECIMAL;
+    } else if (klass == BigInteger.class) {
+      return AttributeType.BIG_INTEGER;
+    } else if (klass == float.class || klass == Float.class) {
+      return AttributeType.FLOAT;
+    } else if (klass == byte.class || klass == Byte.class) {
+      return AttributeType.BYTE;
+    } else if (klass == char.class || klass == Character.class) {
+      return AttributeType.CHAR;
+    } else if (klass == Timestamp.class) {
+      return AttributeType.SQL_TIMESTAMP;
+    } else if (klass == java.sql.Date.class) {
+      return AttributeType.SQL_DATE;
+    } else if (klass == Date.class) {
+      return AttributeType.DATE;
+    } else if (klass.isEnum()) {
+      return AttributeType.ENUM;
     }
+    return null;
+  }
 
-    public static AttributeType getAttributeType(Class klass) {
-        if (klass == String.class) {
-            return AttributeType.STRING;
-        } else if (klass == int.class || klass == Integer.class) {
-            return AttributeType.INTEGER;
-        } else if (klass == long.class || klass == Long.class) {
-            return AttributeType.LONG;
-        } else if (klass == boolean.class || klass == Boolean.class) {
-            return AttributeType.BOOLEAN;
-        } else if (klass == double.class || klass == Double.class) {
-            return AttributeType.DOUBLE;
-        } else if (klass == BigDecimal.class) {
-            return AttributeType.BIG_DECIMAL;
-        } else if (klass == BigInteger.class) {
-            return AttributeType.BIG_INTEGER;
-        } else if (klass == float.class || klass == Float.class) {
-            return AttributeType.FLOAT;
-        } else if (klass == byte.class || klass == Byte.class) {
-            return AttributeType.BYTE;
-        } else if (klass == char.class || klass == Character.class) {
-            return AttributeType.CHAR;
-        } else if (klass == Timestamp.class) {
-            return AttributeType.SQL_TIMESTAMP;
-        } else if (klass == java.sql.Date.class) {
-            return AttributeType.SQL_DATE;
-        } else if (klass == Date.class) {
-            return AttributeType.DATE;
-        } else if (klass.isEnum()) {
-            return AttributeType.ENUM;
-        }
-        return null;
-    }
+  public static void reset() {
+    GETTER_CACHE.clear();
+  }
 
-    public static void reset() {
-        GETTER_CACHE.clear();
-    }
+  public static AttributeType getAttributeType(Object value, String attribute) {
+    return getAttributeType(createGetter(value, attribute).getReturnType());
+  }
 
-    public static AttributeType getAttributeType(Object value, String attribute) {
-        return getAttributeType(createGetter(value, attribute).getReturnType());
-    }
-
-    private static Getter createGetter(Object obj, String attribute) {
+  private static Getter createGetter(Object obj, String attribute) {
         Class clazz = obj.getClass();
         final String cacheKey = clazz.getName() + ":" + attribute;
         Getter getter = GETTER_CACHE.get(cacheKey);
@@ -96,7 +97,9 @@ public final class ReflectionHelper {
         try {
             Getter parent = null;
             List<String> possibleMethodNames = new ArrayList<String>(INITIAL_CAPACITY);
+            StringBuilder runningName = new StringBuilder();
             for (final String name : attribute.split("\\.")) {
+                runningName.append(name).append(".");
                 Getter localGetter = null;
                 possibleMethodNames.clear();
                 possibleMethodNames.add(name);
@@ -105,6 +108,10 @@ public final class ReflectionHelper {
                 possibleMethodNames.add("is" + camelName);
                 if (name.equals(THIS_ATTRIBUTE_NAME)) {
                     localGetter = new ThisGetter(parent, obj);
+                } else if (clazz.equals(Map.class)) {
+                  // TODO Escape dots?
+                  String dotField = attribute.replaceFirst(runningName.toString(), "");
+                  localGetter = new MapEntryGetter(parent, dotField);
                 } else {
                     for (String methodName : possibleMethodNames) {
                         try {
@@ -160,105 +167,143 @@ public final class ReflectionHelper {
         }
     }
 
-    public static Comparable extractValue(Object object, String attributeName) throws Exception {
-        return (Comparable) createGetter(object, attributeName).getValue(object);
+  public static Comparable extractValue(Object object, String attributeName) throws Exception {
+    return (Comparable) createGetter(object, attributeName).getValue(object);
+  }
+
+  private abstract static class Getter {
+    protected final Getter parent;
+
+    public Getter(final Getter parent) {
+      this.parent = parent;
     }
 
-    private abstract static class Getter {
-        protected final Getter parent;
+    abstract Object getValue(Object obj) throws Exception;
 
-        public Getter(final Getter parent) {
-            this.parent = parent;
-        }
+    abstract Class getReturnType();
 
-        abstract Object getValue(Object obj) throws Exception;
+    abstract boolean isCacheable();
+  }
 
-        abstract Class getReturnType();
+  static class MethodGetter extends Getter {
+    final Method method;
 
-        abstract boolean isCacheable();
+    MethodGetter(Getter parent, Method method) {
+      super(parent);
+      this.method = method;
     }
 
-    static class MethodGetter extends Getter {
-        final Method method;
-
-        MethodGetter(Getter parent, Method method) {
-            super(parent);
-            this.method = method;
-        }
-
-        Object getValue(Object obj) throws Exception {
-            Object paramObj = obj;
-            paramObj = parent != null ? parent.getValue(paramObj) : paramObj;
-            return paramObj != null ? method.invoke(paramObj) : null;
-        }
-
-        Class getReturnType() {
-            return this.method.getReturnType();
-        }
-
-        @Override
-        boolean isCacheable() {
-            return THIS_CL.equals(method.getDeclaringClass().getClassLoader());
-        }
-
-        @Override
-        public String toString() {
-            return "MethodGetter [parent=" + parent + ", method=" + method.getName() + "]";
-        }
+    Object getValue(Object obj) throws Exception {
+      Object paramObj = obj;
+      paramObj = parent != null ? parent.getValue(paramObj) : paramObj;
+      return paramObj != null ? method.invoke(paramObj) : null;
     }
 
-    static class FieldGetter extends Getter {
-        final Field field;
-
-        FieldGetter(Getter parent, Field field) {
-            super(parent);
-            this.field = field;
-        }
-
-        @Override
-        Object getValue(Object obj) throws Exception {
-            Object paramObj = obj;
-            paramObj = parent != null ? parent.getValue(paramObj) : paramObj;
-            return paramObj != null ? field.get(paramObj) : null;
-        }
-
-        @Override
-        Class getReturnType() {
-            return this.field.getType();
-        }
-
-        @Override
-        boolean isCacheable() {
-            return THIS_CL.equals(field.getDeclaringClass().getClassLoader());
-        }
-
-        @Override
-        public String toString() {
-            return "FieldGetter [parent=" + parent + ", field=" + field + "]";
-        }
+    Class getReturnType() {
+      return this.method.getReturnType();
     }
 
-    static class ThisGetter extends Getter {
-        final Object object;
-
-        public ThisGetter(final Getter parent, Object object) {
-            super(parent);
-            this.object = object;
-        }
-
-        @Override
-        Object getValue(Object obj) throws Exception {
-            return obj;
-        }
-
-        @Override
-        Class getReturnType() {
-            return this.object.getClass();
-        }
-
-        @Override
-        boolean isCacheable() {
-            return false;
-        }
+    @Override
+    boolean isCacheable() {
+      return THIS_CL.equals(method.getDeclaringClass().getClassLoader());
     }
+
+    @Override
+    public String toString() {
+      return "MethodGetter [parent=" + parent + ", method=" + method.getName() + "]";
+    }
+  }
+
+  static class FieldGetter extends Getter {
+    final Field field;
+
+    FieldGetter(Getter parent, Field field) {
+      super(parent);
+      this.field = field;
+    }
+
+    @Override
+    Object getValue(Object obj) throws Exception {
+      Object paramObj = obj;
+      paramObj = parent != null ? parent.getValue(paramObj) : paramObj;
+      return paramObj != null ? field.get(paramObj) : null;
+    }
+
+    @Override
+    Class getReturnType() {
+      return this.field.getType();
+    }
+
+    @Override
+    boolean isCacheable() {
+      return THIS_CL.equals(field.getDeclaringClass().getClassLoader());
+    }
+
+    @Override
+    public String toString() {
+      return "FieldGetter [parent=" + parent + ", field=" + field + "]";
+    }
+  }
+
+  static class ThisGetter extends Getter {
+    final Object object;
+
+    public ThisGetter(final Getter parent, Object object) {
+      super(parent);
+      this.object = object;
+    }
+
+    @Override
+    Object getValue(Object obj) throws Exception {
+      return obj;
+    }
+
+    @Override
+    Class getReturnType() {
+      return this.object.getClass();
+    }
+
+    @Override
+    boolean isCacheable() {
+      return false;
+    }
+  }
+
+  static class MapEntryGetter extends Getter {
+    final String dotField;
+
+    public MapEntryGetter(final Getter parent, String dotField) {
+      super(parent);
+      this.dotField = dotField;
+    }
+
+    @Override
+    Object getValue(Object obj) throws Exception {
+      Object paramObj = obj;
+      paramObj = parent != null ? parent.getValue(paramObj) : paramObj;
+      return paramObj != null ? getValueRecursive((Map<String,Object>)paramObj, dotField) : null;
+      
+    }
+    
+    private Object getValueRecursive(Map<String, Object> inMap, String field) {
+      String[] current = field.split("\\.");
+      if (current.length == 1) {
+        return inMap.get(field);
+      } else {
+        Map<String, Object> outMap = (Map<String,Object>)inMap.get(current[0]);
+        String outField = field.replaceFirst(current[0] + "\\.", "");        
+        return getValueRecursive(outMap,outField); 
+      }           
+    }
+
+    @Override
+    Class getReturnType() {
+      return Object.class;
+    }
+
+    @Override
+    boolean isCacheable() {
+      return false;
+    }
+  }
 }
